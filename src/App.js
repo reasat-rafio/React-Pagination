c21dcc3ccdcc2972ee46bff6b7b00489021c3a73
Reasat RafiO <rafioar96@gmail.com>
2020-08-10 20:10:07 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Post from "./Components/Post";
+import Pagination from "./Components/Pagination";
 
 function App() {
+  const [posts, setPost] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postPerPage] = useState(10);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((res) => setPost(res));
+    setLoading(false);
+  }, []);
+
+  //Get current post
+  const indexOfLastPost = currentPage * postPerPage;
+  const indexOfFirstPost = indexOfLastPost - postPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+  // Change page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mb-5">
+      <h1 className="tex-primary" mb-3>
+        My blog
+      </h1>
+      <Post posts={currentPosts} loading={loading} />
+      <Pagination
+        paginate={paginate}
+        postPerPage={postPerPage}
+        totalPosts={posts.length}
+      />
     </div>
   );
 }
